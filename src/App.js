db86d50ca80875d45d6fc923056a15f452cc9a6c
Joseph Yeh <joseph@settle.co>
2022-11-17 13:57:13 -0800

    fix

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  uri: 'https://attain-server.herokuapp.com',
   cache: new InMemoryCache(),
 });
 
